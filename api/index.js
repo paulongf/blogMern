4e1f61e169fc6ce6f0c97a368f28df77e70232c6
Paulongf <paulongf@hotmail.com>
2025-03-13 19:11:45 +0000

    Criando repositório para novo Blog

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+  const {username, password} = req.body;
+  res.json({requestData: {username, password}});
+});
+
+
+app.listen(4000, () => {
+    console.log('🚀 Backend connected on port 4000');
+});
